6c37d5e9852772deccac73abb5a354970a7ae3b0
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-09-15 22:50:55 -0300

    cambios boton quitar

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ let productoCarrito = [];
 
 
 //agregando evento click
-document.addEventListener("click", (e)=>{
+document.addEventListener("click", (e) => {
     console.log(e.target.matches(".btn-outline-primary")); //entrega un false o un true
     agregarCarrito(e);
     // console.log(e.target);
 
-    if(e.target.matches(".btnSumar")){
+    if (e.target.matches(".btnSumar")) {
 
         sumarProducto(e);
         console.log("Agregar producto");
 
-    }else if(e.target.matches(".btnQuitar")){
-        // quitarProducto(e);
-        console.log("Quitar producto");
+    }
 
+    if (e.target.matches(".btnQuitar")) {
+        quitarProducto(e);
+        console.log("Quitar producto");
     }
 
 });
@@ -30,27 +31,27 @@ document.addEventListener("click", (e)=>{
 
 // funcion para agregar elementos al carrito
 
-const agregarCarrito = (e)=>{ //pasamos el evento para
+const agregarCarrito = (e) => { //pasamos el evento para
 
-     const producto = {
+    const producto = {
         nombre: e.target.dataset.fruta,
-        id:e.target.id,
-        cantidad : 1,
-        precio:parseInt(e.target.dataset.precio)
-     }
-     const index = productoCarrito.findIndex(item => item.id === producto.id); //recorre el array comparando ids
-     console.log(index);
-     if (index === -1){
+        id: e.target.id,
+        cantidad: 1,
+        precio: parseInt(e.target.dataset.precio)
+    }
+    const index = productoCarrito.findIndex(item => item.id === producto.id); //recorre el array comparando ids
+    console.log(index);
+    if (index === -1) {
         productoCarrito.push(producto);
-     }else{
+    } else {
         productoCarrito[index].cantidad++;
-     }
+    }
     //  console.log(producto);
     //  console.log(e.target.dataset.id);
-    
 
 
-    pintarCarrito(producto);
+
+    pintarCarrito(productoCarrito);
 
 
 }
@@ -59,39 +60,59 @@ const agregarCarrito = (e)=>{ //pasamos el evento para
 
 // ahora viene agregar el producto al carrito y pintarlos
 
-const pintarCarrito = () => { 
+const pintarCarrito = () => {
     carrito.textContent = null; //vacio para que no se repita todos los elementos agregados antes
-    const clone = template.content.cloneNode(true);
     
-    productoCarrito.forEach(item =>{
+    productoCarrito.forEach(item => {
         const clone = template.content.cloneNode(true);
         clone.getElementById("nombreProducto").textContent = item.nombre;
         clone.getElementById("cantidad").textContent = item.cantidad;
         clone.getElementById("precioProducto").textContent = item.cantidad * item.precio;
-        console.log(item);
+        clone.querySelector(".btnSumar").dataset.id = item.id;
+        clone.querySelector(".btnQuitar").dataset.id = item.id;
+        // console.log(item);
         fragment.appendChild(clone);
 
     });
 
     carrito.appendChild(fragment);
-    
 
 
-}//
+
+}
 
 
 //solo falta la logica para q se sume y quiten productos ademas de pintar el total.
 
-// const sumarProducto = (e) => {
-   
-//    productoCarrito = productoCarrito.map(item =>{
-//     console.log("agregar producto " + e.target.dataset.id);
+const sumarProducto = (e) => {
 
-//     if (item.id === e.target.id){
-//         item.cantidad++;
-//     }return item;
+    // console.log("sumarProducto ", e.target.id);
 
-//    });
-   
-//    pintarCarrito();
-// }
+    productoCarrito = productoCarrito.map(item => {
+
+        if (item.id === e.target.dataset.id) {
+            item.cantidad++;
+        } return item;
+
+    });
+
+    pintarCarrito();
+}
+
+const quitarProducto = (e) => {
+
+    productoCarrito = productoCarrito.filter(item => {
+       
+        if (item.cantidad > 0) {
+            item.cantidad--;
+
+            if (item.cantidad === 0) {
+                return;
+            }
+        } return item;
+       
+    });
+
+    pintarCarrito();
+
+}
